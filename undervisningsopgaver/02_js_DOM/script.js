e6bd259fe1566f8450675ff0af72a928056cfd6b
Eva Kjær Hansen <eva@Evas-MacBook-Pro.local>
2020-08-25 11:22:09 +0200

    console log af elementer med constant og forskellige selectors

---
+++ undervisningsopgaver/02_js_DOM/script.js
@@ -0,0 +1,23 @@
+const header = document.querySelector("header");
+console.log(header);
+
+
+const titel = document.querySelector("h1");
+console.log(titel);
+
+
+
+const main = document.querySelector("main");
+console.log(main);
+
+
+const section = document.querySelector("section");
+console.log(section);
+
+
+
+
+
+//const info = document.querySelector("#mitElement");
+//console.log(info);
+//document.querySelector("article:first-child")
